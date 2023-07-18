145141f026eb70fcf03f2a9d9e2de33ceb9c01fd
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-07-18 10:52:34 -0600

    agregar evento submit para la busqueda de paises

--- script.js
+++ script.js
@@ -2,3 +2,16 @@
 const formulario = document.getElementById('formulario');
 const tablaResultados = document.getElementById('tablaResultados');
 const estado = document.getElementById('resultado');
+
+// agregar evento submit para la búsqueda de países
+formulario.addEventListener('submit', async (evento) => {
+// Evitar el envío del formulario por defecto y evita que se recargue cuando se haga una busqueda.
+    evento.preventDefault(); 
+    const tipoBusqueda = formulario.tipoBusqueda.value;
+    const valorBusqueda = formulario.valorBusqueda.value;
+  
+    if (valorBusqueda === '') {
+      alert("Debe ingresar un valor de búsqueda");
+      return;
+    }
+});
