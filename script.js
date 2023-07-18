81cd210b414bddc5ba76b161137c6ba3a245cedf
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-07-18 10:58:07 -0600

    creacion de if para determinar el tipo de url a utilizar y definicion del medot get

--- script.js
+++ script.js
@@ -5,13 +5,30 @@ const estado = document.getElementById('resultado');
 
 // agregar evento submit para la búsqueda de países
 formulario.addEventListener('submit', async (evento) => {
-// Evitar el envío del formulario por defecto y evita que se recargue cuando se haga una busqueda.
-    evento.preventDefault(); 
+    // Evitar el envío del formulario por defecto y evita que se recargue cuando se haga una busqueda.
+    evento.preventDefault();
     const tipoBusqueda = formulario.tipoBusqueda.value;
     const valorBusqueda = formulario.valorBusqueda.value;
-  
+
     if (valorBusqueda === '') {
-      alert("Debe ingresar un valor de búsqueda");
-      return;
+        alert("Debe ingresar un valor de búsqueda");
+        return;
     }
+
+    let url = '';
+    //se creo un if para determinar cual url usar a la hora de realizar la busqueda
+    //mediante el selec creado en el documento html
+    if (tipoBusqueda === 'nombre') {
+        url = `https://restcountries.com/v3.1/name/${valorBusqueda}`;
+    } else if (tipoBusqueda === 'idioma') {
+        url = `https://restcountries.com/v3.1/lang/${valorBusqueda}`;
+    }
+//definir el metodo a utilizar y cuando se este realizando la busqueda mostrar en pantalla
+//el mensaje 'se esta realizando la busqueda...'
+    const config = {
+        method: 'GET'
+    };
+
+    estado.innerText = 'Realizando la búsqueda...';
+    tablaResultados.innerHTML = '';
 });
