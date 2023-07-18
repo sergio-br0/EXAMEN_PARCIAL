f851f2e71502d52174a356feda34d3c46fc51ac6
sergio-br0 <rodriguezbolvito4@gmail.com>
2023-07-18 10:49:36 -0600

    referencias a los elementos del formulario y la tabla de resultados

---
+++ script.js
@@ -0,0 +1,4 @@
+// Obtención de referencias a los elementos del formulario y la tabla de resultados
+const formulario = document.getElementById('formulario');
+const tablaResultados = document.getElementById('tablaResultados');
+const estado = document.getElementById('resultado');
